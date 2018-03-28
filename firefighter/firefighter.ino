2bd38ababd0b6f2ec304b5fcc12d2b4eac93147f
Gary Leung <1234gary@users.noreply.github.com>
2018-03-28 14:40:42 -0400

    Added fire tracking code

--- firefighter/firefighter.ino
+++ firefighter/firefighter.ino
@@ -1,77 +1,75 @@
+#include <Servo.h>
 #include <DRV8835MotorShield.h>
 #include <X113647Stepper.h>
 
-DRV8835MotorShield ms;
 const int LEFT = -1;
 const int RIGHT = 1;
 
 static const int STEPS_PER_REVOLUTION = 64 * 32;  
 X113647Stepper myStepper(STEPS_PER_REVOLUTION, 2, 3, 4, 5);
+DRV8835MotorShield ms;
+Servo myservo;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
   myStepper.setSpeed(6.5);
+  myservo.attach(9);
 }
 
+//void myservo.write(pos); where pos between (0, 180) to move servo
+//void myStepper.step(steps); to move stepper
+//void setM1Speed(int speed); to move motor shield wheels 
+//void setM2Speed(int speed) 
+//void setSpeeds(int m1Speed, int m2Speed) 
+//void flipM1(bool flip) 
+//void flipM2(bool flip) 
+//Serial.println("Hello World")
+//sensor_reading = analogRead(A1)
+
+//void loop() {
+//    myservo.write(180);              // tell servo to go to position in variable 'pos'
+//    delay(500);                       // waits 15ms for the servo to reach the position
+//
+//    myservo.write(0);              // tell servo to go to position in variable 'pos'
+//    delay(500);            
+//}
+
+
+int position = 90;
 void loop() {
-  // Scans and aligns robot towards fire before moving forward
-  
-  alignRobot();
-//  ms.setSpeeds(100, 100);
-//  delay(1000);
+  scanForFire(position);
 }
 
 
-int scanForFire() {
-  int sensorLeft = 0, sensorMiddle = 0, sensorRight = 0;
+void scanForFire(int stepPosition) {
+  int sensorLeft, sensorMiddle, sensorRight;
   int fireDirection;
-  int stepPosition = 0;
-  // Stop motor from moving
-  ms.setSpeeds(0, 0);
-
-  while (sensorMiddle <= max(sensorLeft, sensorRight)){
+  
+  while(true){
     sensorLeft = analogRead(A1);
     sensorMiddle = analogRead(A2);
     sensorRight = analogRead(A3);
-    
+
+    if ((sensorMiddle > sensorLeft)&&(sensorMiddle > sensorRight)){
+      continue;
+    }
     if (sensorLeft > sensorRight){
       fireDirection = LEFT;
     }else{
       fireDirection = RIGHT;
     }
-    stepPosition += fireDirection*100;
-    myStepper.step(fireDirection*100);
+    
+    stepPosition = min(max(15*fireDirection+stepPosition, 0),180);
+    myservo.write(stepPosition);
+    delay(500);
+    
     Serial.print("Sensor Values: ");
-    Serial.print(sensorLeft);
-    Serial.print(sensorMiddle);
+    Serial.println(sensorLeft);
+    Serial.println(sensorMiddle);
     Serial.println(sensorRight);
-  }
-  return stepPosition;
+    Serial.println("pos");
+    Serial.println(position);
+    }
 }
 
-void alignRobot(){
-  // Keeps on scanning for fire until vehicle is aligned
-  int error = scanForFire();
-  Serial.println("Error");
-  Serial.println(error);
-//  while (abs(error) > 20){
-//    Serial.println("Error");
-//    Serial.println(error);
-//    if (error > 0){
-//      ms.setSpeeds(0, 100);
-//      delay(100);
-//    }else{
-//      ms.setSpeeds(100, 0);
-//      delay(100);
-//    }
-//    error = scanForFire();
-//  }
-}
-
-//void setM1Speed(int speed) 
-//void setM2Speed(int speed) 
-//void setSpeeds(int m1Speed, int m2Speed) 
-//void flipM1(bool flip) 
-//void flipM2(bool flip) 
-//myStepper.step(STEPS_PER_REVOLUTION);
