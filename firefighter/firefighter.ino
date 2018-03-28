06470569e1c6f077ca5fc8bcdbf26bf3572c5538
Gary Leung <1234gary@users.noreply.github.com>
2018-03-28 00:55:33 -0400

    Updated detection code

--- firefighter/firefighter.ino
+++ firefighter/firefighter.ino
@@ -2,31 +2,76 @@
 #include <X113647Stepper.h>
 
 DRV8835MotorShield ms;
+const int LEFT = -1;
+const int RIGHT = 1;
 
 static const int STEPS_PER_REVOLUTION = 64 * 32;  
-X113647Stepper myStepper(STEPS_PER_REVOLUTION, 8, 9, 10, 11);
+X113647Stepper myStepper(STEPS_PER_REVOLUTION, 2, 3, 4, 5);
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
+  myStepper.setSpeed(6.5);
 }
 
 void loop() {
-  // read the input on analog pin 0:
-  int sensorValue = analogRead(A1);
-  // Convert the analog reading (which goes from 0 - 1023) to a voltage (0 - 5V):
-  float voltage = sensorValue * (5.0 / 1023.0);
-  // print out the value you read:
-  Serial.println(voltage);
-  delay(100);
-  ms.setM1Speed(100);
+  // Scans and aligns robot towards fire before moving forward
+  
+  alignRobot();
+//  ms.setSpeeds(100, 100);
+//  delay(1000);
 }
 
 
+int scanForFire() {
+  int sensorLeft = 0, sensorMiddle = 0, sensorRight = 0;
+  int fireDirection;
+  int stepPosition = 0;
+  // Stop motor from moving
+  ms.setSpeeds(0, 0);
+
+  while (sensorMiddle <= max(sensorLeft, sensorRight)){
+    sensorLeft = analogRead(A1);
+    sensorMiddle = analogRead(A2);
+    sensorRight = analogRead(A3);
+    
+    if (sensorLeft > sensorRight){
+      fireDirection = LEFT;
+    }else{
+      fireDirection = RIGHT;
+    }
+    stepPosition += fireDirection*100;
+    myStepper.step(fireDirection*100);
+    Serial.print("Sensor Values: ");
+    Serial.print(sensorLeft);
+    Serial.print(sensorMiddle);
+    Serial.println(sensorRight);
+  }
+  return stepPosition;
+}
+
+void alignRobot(){
+  // Keeps on scanning for fire until vehicle is aligned
+  int error = scanForFire();
+  Serial.println("Error");
+  Serial.println(error);
+//  while (abs(error) > 20){
+//    Serial.println("Error");
+//    Serial.println(error);
+//    if (error > 0){
+//      ms.setSpeeds(0, 100);
+//      delay(100);
+//    }else{
+//      ms.setSpeeds(100, 0);
+//      delay(100);
+//    }
+//    error = scanForFire();
+//  }
+}
 
 //void setM1Speed(int speed) 
 //void setM2Speed(int speed) 
 //void setSpeeds(int m1Speed, int m2Speed) 
 //void flipM1(bool flip) 
 //void flipM2(bool flip) 
-
+//myStepper.step(STEPS_PER_REVOLUTION);
