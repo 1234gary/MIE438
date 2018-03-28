da8aff4e704c688ccd6ccc4bfce53e6cc6069180
Robert Bazzocchi <robertbazzocchi@gmail.com>
2018-03-28 16:31:50 -0400

    fixed left-right sensor and output

--- firefighter/firefighter.ino
+++ firefighter/firefighter.ino
@@ -1,19 +1,19 @@
 #include <Servo.h>
-#include <DRV8835MotorShield.h>
-#include <X113647Stepper.h>
+//#include <DRV8835MotorShield.h>
+//#include <X113647Stepper.h>
 
 const int LEFT = -1;
 const int RIGHT = 1;
 
 static const int STEPS_PER_REVOLUTION = 64 * 32;  
-X113647Stepper myStepper(STEPS_PER_REVOLUTION, 2, 3, 4, 5);
-DRV8835MotorShield ms;
+//X113647Stepper myStepper(STEPS_PER_REVOLUTION, 2, 3, 4, 5);
+//DRV8835MotorShield ms;
 Servo myservo;
 
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
-  myStepper.setSpeed(6.5);
+//  myStepper.setSpeed(6.5);
   myservo.attach(9);
 }
 
@@ -27,49 +27,52 @@ void setup() {
 //Serial.println("Hello World")
 //sensor_reading = analogRead(A1)
 
-//void loop() {
-//    myservo.write(180);              // tell servo to go to position in variable 'pos'
-//    delay(500);                       // waits 15ms for the servo to reach the position
-//
-//    myservo.write(0);              // tell servo to go to position in variable 'pos'
-//    delay(500);            
-//}
-
-
-int position = 90;
 void loop() {
-  scanForFire(position);
+    myservo.write(180);              // tell servo to go to position in variable 'pos'
+    delay(500);                       // waits 15ms for the servo to reach the position
+ 
+    myservo.write(0);              // tell servo to go to position in variable 'pos'
+    delay(500);            
 }
 
 
-void scanForFire(int stepPosition) {
-  int sensorLeft, sensorMiddle, sensorRight;
-  int fireDirection;
-  
-  while(true){
-    sensorLeft = analogRead(A1);
-    sensorMiddle = analogRead(A2);
-    sensorRight = analogRead(A3);
-
-    if ((sensorMiddle > sensorLeft)&&(sensorMiddle > sensorRight)){
-      continue;
-    }
-    if (sensorLeft > sensorRight){
-      fireDirection = LEFT;
-    }else{
-      fireDirection = RIGHT;
-    }
-    
-    stepPosition = min(max(15*fireDirection+stepPosition, 0),180);
-    myservo.write(stepPosition);
-    delay(500);
-    
-    Serial.print("Sensor Values: ");
-    Serial.println(sensorLeft);
-    Serial.println(sensorMiddle);
-    Serial.println(sensorRight);
-    Serial.println("pos");
-    Serial.println(position);
-    }
-}
+//int position = 90;
+//void loop() {
+//  scanForFire(position);
+//}
+//
+//
+//void scanForFire(int stepPosition) {
+//  int sensorLeft, sensorMiddle, sensorRight;
+//  int fireDirection;
+//  
+//  while(true){
+//    sensorRight = analogRead(A1);
+//    sensorMiddle = analogRead(A2);
+//    sensorLeft = analogRead(A3);
+//
+//    if ((sensorMiddle > sensorRight)&&(sensorMiddle > sensorLeft)){
+//      continue;
+//    }
+//    if (sensorRight > sensorLeft){
+//      fireDirection = LEFT;
+//    }else{
+//      fireDirection = RIGHT;
+//    }
+//    
+//    stepPosition = min(max(15*fireDirection+stepPosition, 0),180);
+//    myservo.write(stepPosition);
+//    delay(500);
+//    
+//    Serial.println("____________Sensor Values____________");
+//    Serial.print("Left Sensor: ");
+//    Serial.println(sensorLeft);
+//    Serial.print("Middle Sensor: ");
+//    Serial.println(sensorMiddle);
+//    Serial.print("Right Sensor: ");
+//    Serial.println(sensorRight);
+//    Serial.print("Position:");
+//    Serial.println(position);
+//    }
+//}
 
